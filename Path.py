7779b6c205c741b8e32dc73708894d2332b518c0
eydissjofn <eydis_sjofn@hotmail.com>
2022-11-04 01:29:25 +0100

    weather data

--- Path.py
+++ Path.py
@@ -1,6 +1,9 @@
 #import map
 from math import cos, asin, sqrt, pi
 import SimulationParameters
+import pandas as pd
+import numpy as np
+import random
 
 class Path:
     
@@ -16,14 +19,29 @@ class Path:
     scoutedArea = [[]]
     
     def __init__(self):
+        filename = 'model_windspeed.csv'
+        w_dir, w_speed = self.get_weather_data(filename)
         self.droneRange = SimulationParameters.DetectionRadius
         self.initMap()
         self.mapWidthCoordStep = abs(SimulationParameters.Eborderline - SimulationParameters.Wborderline) / self.mapWidthScaleNR
         self.mapLengthCoordStep = abs(SimulationParameters.Nborderline - SimulationParameters.Sborderline) / self.mapLengthScaleNR
         # Line below works only For positive long and negative lat, if we move from Iceland we have to change that
-        self.scoutedArea = [[Sector(SimulationParameters.Sborderline + j * self.mapLengthCoordStep, SimulationParameters.Wborderline - i * self.mapWidthCoordStep) for j in range(self.mapLengthScale)] for i in range(self.mapWidthScale)]
-        
-        
+        self.scoutedArea = [[Sector(SimulationParameters.Sborderline + j * self.mapLengthCoordStep, SimulationParameters.Wborderline - i * self.mapWidthCoordStep, random.choice(w_speed), random.choice(w_dir)) for j in range(self.mapLengthScale)] for i in range(self.mapWidthScale)]
+
+    def get_weather_data(self, filename):
+        weather_cvs = pd.read_csv(filename, encoding='latin-1')
+        raw_data = weather_cvs.values
+        cols1 = range(2, 4)
+        X1 = raw_data[:, cols1]
+        cols2 = range(4, 6)
+        X2 = raw_data[:, cols2]
+        cols3 = range(6, 8)
+        X3 = raw_data[:, cols3]
+        wind_direction = np.concatenate((X1[:,0],X2[:,0],X3[:,0]))
+        wind_speed = np.concatenate((X1[:,1],X2[:,1],X3[:,1]))
+        return wind_direction, wind_speed
+
+
     # In this function map is divided into search areas which size is defined by dron detection radius
     def initMap(self):
          # Calculate horizontal distance of the search area
@@ -79,13 +97,17 @@ class Sector:
     longitude = 0
     latitiude = 0
     status = "not initialized"
-    windspeed = 0
+    wind_speed = 0
+    wind_direction = 'C'
     
     
-    def __init__(self, long, lat):
+    def __init__(self, long, lat, speed, dir):
         self.longitude = long
         self.latitiude = lat
         self.status = "not scouted"
+        self.wind_speed = speed
+        self.wind_direction = dir
+
         
 # Path flow
 # - Get current drone location
